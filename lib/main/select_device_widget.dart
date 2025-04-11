7ed5654bd48ef01c4974cbbf832d3a4f642fcefe
Andrea Nielsen <andreanielsen83@icloud.com>
2025-04-11 09:41:54 +0200

    mDNS using nsd plugin

--- lib/main/select_device_widget.dart
+++ lib/main/select_device_widget.dart
@@ -1,7 +1,9 @@
 import 'dart:collection';
+import 'dart:io';
 
 import 'package:currently_local/model/device.dart';
 import 'package:flutter/material.dart';
+import 'package:nsd/nsd.dart';
 import 'package:provider/provider.dart';
 
 class SelectDeviceWidget extends StatefulWidget {
@@ -14,10 +16,14 @@ class SelectDeviceWidget extends StatefulWidget {
 class _SelectDeviceWidgetState extends State<SelectDeviceWidget> {
   final _available = SplayTreeMap<String, Device>();
   Device? _selected;
+  Discovery? _discovery;
+  static final String serviceName =
+      Platform.isAndroid ? "_currently._tcp" : "_currently._tcp.";
 
   @override
   void initState() {
-    _addAndListen(Device(deviceId: "b8b41879cf58", localIP: "10.0.1.122"));
+    _startMDnsClient();
+    // _addAndListen(Device(deviceId: "b8b41879cf58", localIP: "10.0.1.122"));
     super.initState();
   }
 
@@ -27,6 +33,34 @@ class _SelectDeviceWidgetState extends State<SelectDeviceWidget> {
       setState(() {
         _available[device.deviceId] = device;
         _selected ??= device;
+      });
+    });
+  }
+
+  Future<void> _startMDnsClient() async {
+    final serviceNameRegexp = RegExp(r'CurrentlyOne_([0-9a-fA-F]+)');
+
+    _discovery?.dispose();
+
+    final discovery =
+        startDiscovery(serviceName, ipLookupType: IpLookupType.v4);
+    discovery.then((d) {
+      _discovery = d;
+      d.addServiceListener((service, status) {
+        final match = serviceNameRegexp.firstMatch(service.name ?? "");
+        final id = match?.group(1);
+        if (null != id && null != service.addresses) {
+          String ipv4 = service.addresses!
+              .firstWhere((element) => element.type == InternetAddressType.IPv4)
+              .address;
+          if (ServiceStatus.found == status) {
+            debugPrint('+ found $id at $ipv4');
+            _addAndListen(Device(deviceId: id, localIP: ipv4,));
+          } else {
+            debugPrint('- lost $id at $ipv4');
+            // _onLostDevice(id, ipv4);
+          }
+        }
       });
     });
   }
